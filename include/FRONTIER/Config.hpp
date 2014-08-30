966ef0af474a8b533da0e6b2f65205923874646b
Frontier789 <fr0nt13r789@gmail.com>
2014-08-30 21:04:58 +0200

    definitions for solaris and irix
    and fixed redefinition warnings

--- include/FRONTIER/Config.hpp
+++ include/FRONTIER/Config.hpp
@@ -20,21 +20,47 @@
 	defined( _WIN64) 	 || \
 	defined(__WIN32__)   || \
 	defined(__TOS_WIN__) || \
-	defined(__WINDOWS__)
+	defined(__WINDOWS__) || defined(FRONTIER_OS_WINDOWS)
 		
 		#ifndef NOMINMAX
 			#define NOMINMAX
 		#endif
-		#define FRONTIER_OS_WINDOWS
+		#ifndef FRONTIER_OS_WINDOWS
+			#define FRONTIER_OS_WINDOWS
+		#endif
 		#ifndef FRONTIER_OS_KNOWN
             #define FRONTIER_OS_KNOWN
         #endif
 
 #endif
 
-#if defined(__ANDROID__)
+#if defined(__ANDROID__) || defined(FRONTIER_OS_ANDROID)
+		
+		#ifndef FRONTIER_OS_ANDROID
+			#define FRONTIER_OS_ANDROID
+		#endif
+		#ifndef FRONTIER_OS_KNOWN
+            #define FRONTIER_OS_KNOWN
+        #endif
 
-		#define FRONTIER_OS_ANDROID
+#endif
+
+#if defined(sun) || defined(__sun) || defined(FRONTIER_OS_SOLARIS)
+
+		#ifndef FRONTIER_OS_SOLARIS
+			#define FRONTIER_OS_SOLARIS
+		#endif
+		#ifndef FRONTIER_OS_KNOWN
+            #define FRONTIER_OS_KNOWN
+        #endif
+
+#endif
+
+#if defined(sgi) || defined(__sgi) || defined(FRONTIER_OS_IRIX)
+
+		#ifndef FRONTIER_OS_IRIX
+			#define FRONTIER_OS_IRIX
+		#endif
 		#ifndef FRONTIER_OS_KNOWN
             #define FRONTIER_OS_KNOWN
         #endif
@@ -43,9 +69,11 @@
 
 #if defined(__linux__) || \
     defined(  linux  ) || \
-    defined(__linux  )
-
-		#define FRONTIER_OS_LINUX
+	defined(__linux  ) || defined(FRONTIER_OS_LINUX)
+		
+		#ifndef FRONTIER_OS_LINUX
+			#define FRONTIER_OS_LINUX
+		#endif
 		#ifndef FRONTIER_OS_KNOWN
             #define FRONTIER_OS_KNOWN
         #endif
@@ -54,9 +82,11 @@
 
 #if defined(macintosh) || \
     defined(Macintosh) || \
-   (defined(__APPLE__) && defined(__MACH__))
-
-		#define FRONTIER_OS_MACOS
+   (defined(__APPLE__) && defined(__MACH__)) || defined(FRONTIER_OS_MACOS)
+		
+		#ifndef FRONTIER_OS_MACOS
+			#define FRONTIER_OS_MACOS
+		#endif
 		#ifndef FRONTIER_OS_KNOWN
             #define FRONTIER_OS_KNOWN
         #endif
@@ -66,9 +96,11 @@
 #if defined(  MSDOS  ) || \
     defined(__MSDOS__) || \
     defined( _MSDOS  ) || \
-    defined(  __DOS__)
-
-        #define FRONTIER_OS_DOS
+	defined(  __DOS__) || defined(FRONTIER_OS_DOS)
+		
+		#ifndef FRONTIER_OS_DOS
+			#define FRONTIER_OS_DOS
+		#endif
 		#ifndef FRONTIER_OS_KNOWN
             #define FRONTIER_OS_KNOWN
         #endif
@@ -77,6 +109,10 @@
 
 #ifndef FRONTIER_OS_KNOWN
 		#define FRONTIER_OS_UNKNOWN
+#endif
+
+#if defined(FRONTIER_OS_ANDROID) && defined(FRONTIER_OS_LINUX)
+	#undef FRONTIER_OS_LINUX
 #endif
 
 ////////////////////////////////////////////////////////////
