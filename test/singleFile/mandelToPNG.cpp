38b48cb8e30dcbdff0c19912b19168c377c3bff3
Frontier789 <fr0nt13r789@gmail.com>
2017-06-16 11:34:45 +0200

    Added smoothness to mandelToPNG

--- test/singleFile/mandelToPNG.cpp
+++ test/singleFile/mandelToPNG.cpp
@@ -38,7 +38,7 @@ public:
 		m_iterations = its;
 	}
 	
-	Size getIterationsOnPlane(vec2d pOnPlane)
+	float getRelativeIterationsOnPlane(vec2d pOnPlane)
 	{
 		auto compPow2 = [](vec2d a) -> vec2d {
 			return vec2d(a.x*a.x - a.y*a.y,2*a.x*a.y);
@@ -48,19 +48,23 @@ public:
 		vec2d c = pOnPlane;
 		C(m_iterations)
 		{
-			if (z.LENGTH() >= 2*2) return i;
-			
+			if (z.LENGTH() >= 2*2)
+			{
+				float smoothval = i + 1 - log(log(z.length()))/log(2.f);
+				return smoothval / m_iterations;
+			}
+
 			z = compPow2(z) + c; 
 		}
-		
-		return m_iterations;
+
+		return 1;
 	}
 
 	Color getColorOnPlane(vec2d pOnPlane)
 	{
-		float its = getIterationsOnPlane(pOnPlane);
-		
-		float ratio = its / float(m_iterations);
+		float ratio = getRelativeIterationsOnPlane(pOnPlane);
+		if (ratio > 1) ratio = 1;
+		if (ratio < 0) ratio = 0;
 		
 		return Color::Black * (1.0 - ratio) + Color::White * (ratio);
 	}
@@ -85,7 +89,7 @@ public:
 
 int main()
 {
-	MandelPlotter plotter(vec2(1200,800),0.003,vec2(-0.5,0),42);
+	MandelPlotter plotter(vec2(1280,768),0.003,vec2(-0.5,0),30);
 	
 	Image img = plotter.render();
 	
